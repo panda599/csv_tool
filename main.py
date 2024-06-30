0b17e4945e66d2f785820cf33b6841c570116d9c
panda599 <panda599@126.com>
2024-06-30 22:17:40 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import streamlit as st
+from utils import dataframe_agent
+
+def create_chart(input_data, chart_type):
+    df_data = dataframe_agent(input_data["data"], columns = input_data["columns"])
+    df_data.set_index(input_data["columns"][0],inplace = True)
+    if chart_type == "bar":
+        st.bar_chart(df_data)
+    elif chart_type == "line":
+        st.line_chart(df_data)
+    elif chart_type == "scatter":
+        st.scatter_chart(df_data)
+
+st.title("CSV数据分析智能工具")
+
+data = st.file_uploader("上传你的数据文件（CSV格式）：", type ="csv")
+if data:
+    st.session_state["df"] = pd.read_csv(data)
+    with st.expander("原始数据"):
+        st.dataframe(st.session_state["df"])
+query = st.text_area("请输入你的问题：")
+button = st.button("生成回答")
+if button and "df" not in st.session_state:
+    st.info("请先上传数据文件")
+if button and "df" in st.session_state:
+   with st.spinner("AI正在思考中，请稍等..."):
+        response_dict = dataframe_agent(st.session_state["df"] , query)
+        if "answer" in response_dict:
+            st.write(response_dict["answer"])
+        if "table" in response_dict:
+            st.table(pd.DataFrame(response_dict["table"]["data"] , columns = response_dict["table"]["columns"]))
+        if "bar" in response_dict:
+            create_chart(response_dict["bar"],"bar")
+        if "line" in response_dict:
+            create_chart(response_dict["line"], "line")
+        if "scatter" in response_dict:
+            create_chart(response_dict["scatter"], "scatter")
